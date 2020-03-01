33a4d8519fdeb07bee2ac83b197ccea40c15d93a
dengyc <dengyc2013@gmail.com>
2020-03-01 15:10:43 +0800

    导出 useDialog

--- packages/react-dialog-context/index.js
+++ packages/react-dialog-context/index.js
@@ -1,3 +1,5 @@
 export * from "./dialog";
 export * from "./ReactDialog";
+export * from "./context";
+export * from "./useDialog";
 //# sourceMappingURL=index.js.map
--- src/react-dialog-context/index.ts
+++ src/react-dialog-context/index.ts
@@ -3,3 +3,7 @@ export * from "./types";
 export * from "./dialog";
 
 export * from "./ReactDialog";
+
+export * from "./context";
+
+export * from "./useDialog";
